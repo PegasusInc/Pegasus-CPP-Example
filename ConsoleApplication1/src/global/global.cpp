b521eb73fe700c5ef56554575b06014212d085b4
PegasusInc <147206452+PegasusInc@users.noreply.github.com>
2023-10-06 23:40:11 +0300

    Update global.cpp

--- ConsoleApplication1/src/global/global.cpp
+++ ConsoleApplication1/src/global/global.cpp
@@ -1,8 +1,8 @@
 #include "global.hpp"
 
 namespace Global::Vars {
-	std::string g_Url = ("http://pauth.online/endpoints/test.php/");
-	std::string g_New = ("http://pauth.online/updates/eek.exe");
+	std::string g_Url = ("http://pauth.online");
+	std::string g_New = ("http://pauth.online");
 	CSimpleIniA g_AuthIni;
 	std::string g_Username;
 	std::string g_Token;
